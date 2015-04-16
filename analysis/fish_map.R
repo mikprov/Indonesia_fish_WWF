3ae3166ec07047a984f54dd6ba7ba36259b4bb91
Andrew Tredennick <atredenn@gmail.com>
2015-04-16 14:24:45 -0400

    Testing kmeans clustering

--- analysis/fish_map.R
+++ analysis/fish_map.R
@@ -1,16 +1,41 @@
-##  Indonesia fish data
+##  Indonesia fish data initial spatial dependence analysis
 
+##  Date:     4-16-2015
+##  Authors:  Andrew Tredennick
+##            Mikaela Provost
+
+####
+####  Load libraries --------------------------------------------
+####
 library(ggmap)
-fish_data <- read.csv("Fish_and_contextual_var_for_SAMSI.csv")
-fish_pts <- fish_data[,c('Lat', 'Lon')]
+library(cluster)
+library(ggplot2)
 
+
+####
+####  Bring in data ---------------------------------------------
+####
+fish_data <- read.csv("../data/Fish_and_contextual_var_for_SAMSI.csv")
+
+
+####
+####  Look at spatial data --------------------------------------
+####
+fish_pts <- fish_data[,c('Lat', 'Lon')]
+plot(fish_pts$Lon, fish_pts$Lat)
+removes <- which(is.na(fish_pts))
+ks <- kmeans(fish_pts[-removes,], centers = 20)
+points(ks$centers[,"Lon"], ks$centers[,"Lat"], col="blue", pch=19)
+str(ks$cluster)
+
+
+####
+####  Make a ggmap ---------------------------------------------
+####
 # Now get the base map using 'get_map', the location is also a Google search
-mapALL <- get_map(location="indonesia", source ="stamen", maptype="toner",
-                  zoom=6)
+mapALL <- get_map(location="selat dampier", source ="stamen", maptype="toner",
+                  zoom=7)
 # Put it together using 'ggmap' and the standard ggplot call to geom_point
 ggmap(mapALL, extent="device")+
-  geom_point(data=fish_data, aes(x=Lon, y=Lat, color=Regional.Location), 
-             size=4, alpha=0.6)
-#   geom_point(data=allD, aes(x=Long, y=Lat),  shape=1, size=4)+
-#   scale_color_discrete(name="Region")
-#   geom_text(data=allD, aes(x=Long, y=Lat, label=Exclosure.Name),hjust=0, vjust=0)
+  geom_point(data=fish_data, aes(x=Lon, y=Lat), 
+             size=4, alpha=0.6, color="dodgerblue")
