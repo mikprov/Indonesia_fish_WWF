23ec3be2a7b314ec8cedf2a64fa5d26e7eac6828
Andrew Tredennick <atredenn@gmail.com>
2015-06-29 23:03:53 -0600

    adds dummy variable set up and parallelization; closes #4

--- analysis/preliminary_fitting.R
+++ analysis/preliminary_fitting.R
@@ -36,6 +36,12 @@ variables <- read.xlsx("/Users/atredenn/Dropbox/Fish Drivers Paper/DATA/contextu
 tmpids <- which(names(variables) %in% c("Site.ID", "Lat", "Lon"))
 tokeep <- c(tmpids, grep("X", names(variables)))
 variables_sub <- variables[,tokeep]
+torm <- which(names(variables_sub) %in% c("X4_Exposure_numeric_code", 
+                                         "X5_reef_slope_numeric_code", 
+                                         "X6_reef_type_num"))
+variables_sub <- variables_sub[,-torm]
+variables_sub$X10_watershed_pollution_risk <- as.factor(variables_sub$X10_watershed_pollution_risk)
+variables_sub$X11_monsoon_direction <- as.factor(variables_sub$X11_monsoon_direction)
 
 ##  Merge data frames
 all_data <- merge(biomass, variables_sub, by.x = "site_id", by.y = "Site.ID")
@@ -66,7 +72,7 @@ parameters{
   vector[yrs_n] a;
   //real b1_mu;
   //vector[fam_n] b1;
-  vector[fam_n] b_mu;
+  vector[covs_n] b_mu;
   matrix[fam_n, covs_n] b;
   vector[fam_n] fint;
   real<lower=0.00001> sig_a;
@@ -89,14 +95,14 @@ transformed parameters{
 }
 model{
   // Priors
-  a_mu ~ uniform(-100,100);
+  a_mu ~ normal(0,1000);
   a ~ normal(a_mu, sig_a);
   //b1_mu ~ uniform(-100,100);
   //  for(f in 1:fam_n){
   //    b1[f] ~ normal(b1_mu, sig_b1);
   //  }
   for(k in 1:covs_n){
-    b_mu[k] ~ uniform(-100,100);
+    b_mu[k] ~ normal(0,10);
     sig_b[k] ~ cauchy(0,5);
     for(f in 1:fam_n)
       b[f,k] ~ normal(b_mu[k], sig_b[k]);
@@ -117,18 +123,23 @@ all_data <- all_data[which(all_data$value>0),]
 x_matrix <- all_data[,grep("X", names(all_data))]
 toscale <- sapply(x_matrix, is.numeric)
 x_matrix[toscale] <- lapply(x_matrix[toscale], scale)
-factors <- sapply(x_matrix, is.factor)
-x_matrix[factors] <- lapply(x_matrix[factors], as.numeric)
-x_matrix <- as.matrix(x_matrix)
+all_data[,grep("X", names(all_data))] <- x_matrix
 
+## Set up design matrix for predictors
+preds <- paste(names(all_data[9:19]), sep="", collapse="+") 
+model <- paste("value", preds, sep="~")
+X <- model.matrix(as.formula(model) , data = all_data)
+
+## Declare variables for STAN
 obs_n <- nrow(all_data)
 yrs_n <- length(unique(all_data$year))
 yid <- as.numeric(as.factor(all_data$year))
-covs_n <- ncol(x_matrix)
 fam_n <- length(unique(all_data$variable))
 fid <- as.numeric(as.factor(all_data$variable))-1
 Y <- log(all_data$value)
-X <- x_matrix
+X <- X[,2:ncol(X)] #take out the added intercept column, we do that ourselves
+covs_n <- ncol(X)
+
 
 ## Set reasonable initial values for three chains
 inits <- list()
@@ -136,19 +147,37 @@ inits[[1]] <- list(a_mu = 0, a=rep(0,yrs_n), b_mu=rep(0, covs_n),
                    b=matrix(0,nrow=fam_n,ncol=covs_n),
                    fint = rep(0,fam_n), sig_b=rep(0.5,covs_n), 
                    sig_a=0.5, sig_m=0.5, sig_f=0.5)
+inits[[2]] <- list(a_mu = 1, a=rep(1,yrs_n), b_mu=rep(1, covs_n), 
+                   b=matrix(1,nrow=fam_n,ncol=covs_n),
+                   fint = rep(1,fam_n), sig_b=rep(0.05,covs_n), 
+                   sig_a=0.05, sig_m=0.05, sig_f=0.05)
+inits[[3]] <- list(a_mu = 0.5, a=rep(0.5,yrs_n), b_mu=rep(0.5, covs_n), 
+                   b=matrix(0.5,nrow=fam_n,ncol=covs_n),
+                   fint = rep(0.5,fam_n), sig_b=rep(0.25,covs_n), 
+                   sig_a=0.25, sig_m=0.25, sig_f=0.25)
 
 datalist <- list(obs_n=obs_n, yrs_n=yrs_n, yid=yid, covs_n=covs_n,
                  fam_n=fam_n, fid=fid, Y=Y, X=X)
-pars=c("a_mu", "a", "b_mu",  "b", "sig_b", "sig_a", "sig_m", "fint", "sig_f")
+pars=c("a_mu", "a", "b_mu",  "b", "sig_b", "sig_a",
+       "sig_m", "fint", "sig_f")
 
 stanmodel <- stan(model_code=model_string, data=datalist, pars=pars, chains=0)
 
-mcmc_samples <- stan(fit=stanmodel, data=datalist, init=inits,
-                     pars=pars, chains=1, iter=2000, warmup=1000)
-print(mcmc_samples)
-plot(mcmc_samples)
-traceplot(mcmc_samples)
+# mcmc_samples <- stan(fit=stanmodel, data=datalist, init=inits,
+#                      pars=pars, chains=1, iter=1000, warmup=500)
+# print(mcmc_samples)
+# plot(mcmc_samples)
+# traceplot(mcmc_samples)
 
+##  Run MCMC in parallel
+rng_seed <- 123
+sflist <-
+  mclapply(1:3, mc.cores=3,
+           function(i) stan(fit=stanmodel, data=datalist, pars=pars,
+                            seed=rng_seed, chains=1, chain_id=i, refresh=-1,
+                            iter=2000, warmup=1000, init=list(inits[[i]])))
+fit <- sflist2stanfit(sflist)
+print(fit)
 
 
 
