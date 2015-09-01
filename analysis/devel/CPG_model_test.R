139dd32254140dbc2540a30e8c7474b902ef38a8
Andrew Tredennick <atredenn@gmail.com>
2015-09-01 09:35:57 -0600

    updates model outputs

--- analysis/devel/CPG_model_test.R
+++ analysis/devel/CPG_model_test.R
@@ -11,16 +11,17 @@ library(rjags); library(ggplot2)
 library(plyr);  library(reshape2)
 
 
+
 ####
 ####  Simulate data
 ####
 set.seed(123)
 n.site <- 100
-alpha0 <- log(1.1)
-sigma.s <- 0.25
-alpha <- 200
-beta <- 2
-Mu <- rnorm(n.site, exp(alpha0), sigma.s)
+alpha0 <- -1
+sigma <- 1
+alpha <- 0.4
+beta <- 0.1
+Mu <- exp(alpha0 + rnorm(n.site,0,sigma))
 N.s <- rpois(n.site, Mu)
 N.s
 Y.s <- N.s
@@ -39,7 +40,8 @@ E.pres.prob <- 1-exp(-Mu)
 mean(1-E.pres.prob)
 E.pos.biomass <- ((Mu*alpha)/beta)*(1/(1-exp(-Mu)))
 E.biomass <- (Mu*alpha)/beta
-
+mean(E.biomass)
+mean(E.pos.biomass)
 
 ####
 ####  Write JAGS model
@@ -56,7 +58,6 @@ model{
   for(k in 1:npres){
     ## Number of patches (latent)
     npatch[k] ~ dpois(mupred[pres[k]]) T(1,)
-    
     ## Observed positive biomass
     Y_a[k] <- a*npatch[k]
     Y[pres[k]] ~ dgamma(Y_a[k], b)
@@ -65,7 +66,7 @@ model{
   ## Evaluation of probabilities of zeros
   for(j in 1:nabs){
     # Probability of presence at site j
-    proba[j] <- 1 - exp(-mupred[abse[j]])
+    proba[j] <- 1-exp(-mupred[abse[j]])
     Y[abse[j]] ~ dbern(proba[j])
   }
 
@@ -79,16 +80,8 @@ model{
   }
 
   ### Derived quantities
-  for(i in 1:nsite){
-    mu.new[i] <- exp(alpha + eps[i])
-    ngis.new[i] ~ dpois(mu.new[i])
-    y.new ~ ifelse()    
-if(ngis.new[i]>0){y.new[i] ~ dgamma(a*ngis.new[i], b)}
-    if(ngis.new[1]==0){y.new[i] <- 0}
-  }
-
-  
-}" #end model
+  avgposbiom <- a/b
+} #end model" 
 
 
 
@@ -99,15 +92,18 @@ abse <- which(Y.data[,"biomass"]==0)
 pres <- which(Y.data[,"biomass"]>0)
 datalist <- list(Y=Y.data[,"biomass"], nsite=nrow(Y.data),
                  abse=abse, pres=pres, nabs=length(abse), npres=length(pres))
-pars <- c("alpha", "a", "b", "y.new")
+pars <- c("alpha", "a", "b", "npatch", "avgposbiom")
 
-nAdapt <- 1000
-nIter <- 20000
-nSamp <- 20000
+nAdapt <- 100
+nIter <- 1000
+nSamp <- 2000
 jm1 <- jags.model(textConnection(model.string), data=datalist, n.chains=1, n.adapt = nAdapt)
 update(jm1, n.iter=nIter)
-zm <- coda.samples(jm1, variable.names=pars, n.iter=nSamp, n.thin=100)
+zm <- coda.samples(jm1, variable.names=pars, n.iter=nSamp, n.thin=1)
 
-
-
-
+zmd <- as.data.frame(zm[[1]])
+par(mfrow=c(1,3))
+plot(density(zmd[,"alpha"]))
+plot(density(zmd[,"a"]))
+plot(density(zmd[,"b"]))
+median(zmd[,"alpha"])
